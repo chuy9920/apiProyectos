05bec8312aa9a49ccdf08a00b76ed85921d8ea02
Your Name <14658@utcv.edu.mx>
2019-09-03 02:11:11 -0500

    Configuracion

--- index.js
+++ index.js
@@ -3,19 +3,20 @@
 // Se requiere el módulo de mongoose
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 3700;
+
+app.set('port', process.env.PORT || 3000)
 // Se hará una promesa
 mongoose.Promise = global.Promise;
 
 // El método connect (Que recibe la cadena de conexión) se encargará de establecerla
-mongoose.connect('mongodb+srv://chuy99:chuy99@cluster0-quhju.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://chuy99:chuy99@cluster0-quhju.mongodb.net/test?retryWrites=true&w=majority')
     // El método then recibe función de callback
     .then(() => {
         console.log("Conexion a la base de datos establecida satisfactoriamente...");
 
         // Creación del servidor
-        app.listen(port, () => {
-            console.log("Servidor corriendo correctamente en la url localhost:3700")
+        app.listen(app.get('port'), () => {
+            console.log("Servidor corriendo correctamente en la url localhost: ", app.get('port'));
         });
 
     })
